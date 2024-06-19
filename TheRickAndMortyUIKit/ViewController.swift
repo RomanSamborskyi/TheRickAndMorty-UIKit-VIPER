cc4f0e00664260247e76220e5d46ddb15b4ad9ab
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2024-06-19 18:35:45 +0200

    First commit

---
+++ TheRickAndMortyUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheRickAndMortyUIKit
+//
+//  Created by Roman Samborskyi on 19.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
